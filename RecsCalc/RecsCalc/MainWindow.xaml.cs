78e5a94ecca07891adb0adce16607c3675867c80
Sören Johanson <soren@johanson-net.me>
2017-02-22 16:14:43 +0100

    Revert "Final Release"
    This reverts commit dbbe3d031e8393dd64f3107a886adea2b8775744.

---
+++ RecsCalc/RecsCalc/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace RecsCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnBerechnen_Click(object sender, RoutedEventArgs e)
+        {
+            double totalTF = int.Parse(txtDuraAmount.Text) + int.Parse(txtTibaAmount.Text) + int.Parse(txtKrisAmount.Text) + int.Parse(txtEZAmount.Text);
+            double RecStorage = 17500;
+            double recsNeeded = System.Math.Round(totalTF / RecStorage);
+            lblMinText.Content = recsNeeded.ToString();
+            lblRecText.Content = (recsNeeded + 5).ToString();
+        }
+    }
+}
